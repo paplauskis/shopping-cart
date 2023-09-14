22caab0c0ad6ee3f35f4dbe9f0741a857d8954de
paplauskis <paplauskaskostas@gmail.com>
2023-09-14 18:20:54 +0300

    created Description component

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,12 @@
+import './Description.css'
+
+function Description() {
+  return (
+    <div className="container">
+      <h2>Welcome to RNDMshop!</h2>
+      <h3>Here You Will find a Lot of Random Products That Will Probably be Worth a Purchase</h3>
+    </div>
+  )
+}
+
+export default Description
