4a574b33d2c4eeefc8bbe95c6989c6cdf6dbc14f
paplauskis <paplauskaskostas@gmail.com>
2023-09-14 18:54:43 +0300

    added css module

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,13 +1,13 @@
-import './NavBar.css'
+import navBar from './NavBar.module.css'
 import cartImage from '../../assets/cart.png';
 
 function NavBar() {
   return (
     <header>
       <h1>RNDMshop</h1>
-      <div className="links">
+      <div className={navBar.links}>
         <a href="/">Shop</a>
-        <button><img src={cartImage} alt="shopping cart icon" /> </button>
+        <a href='/'><img src={cartImage} alt="shopping cart icon" /> </a>
       </div>
     </header>
   )
