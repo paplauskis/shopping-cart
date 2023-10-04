eba90474c94606061bf352a217d9c05995ef0b2a
paplauskis <paplauskaskostas@gmail.com>
2023-10-04 11:52:55 +0300

    made image slider functional

--- src/components/photoReel/PhotoReel.jsx
+++ src/components/photoReel/PhotoReel.jsx
@@ -6,16 +6,35 @@ import tshirts from '../../assets/cotton-tees.jpg'
 import memoryDrive from '../../assets/memory-drive.jpg'
 import monitor from '../../assets/monitor-setup.jpg'
 import winterCoat from '../../assets/winter-coat.jpg'
-
+import { useState } from 'react'
 
 function PhotoReel() {
+  const [currentImg, setCurrentImg] = useState(0)
+  const allImgs = [jacket, tshirts, memoryDrive, monitor, winterCoat]
+  const length = allImgs.length
+
+  const nextImg = () => {
+    setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1)
+  }
+
+  const prevImg = () => {
+    setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1)
+  }
+
   return (
     <div className={photoReel.container}>
-      <button className={photoReel.previous}>
-      <img src={backBtn} alt="back button" />
+      <button className={photoReel.previous} onClick={prevImg}>
+        <img src={backBtn} alt='back button' />
       </button>
-      <button className={photoReel.next}>
-      <img src={nextBtn} alt="next button" />
+      {allImgs.map((img, index) => {
+        return (
+          <div className={index === currentImg ? 'visible' : 'hidden'} key={index}>
+            {index === currentImg && <img src={allImgs[currentImg]} alt='image' />}
+          </div>
+        )
+      })}
+      <button className={photoReel.next} onClick={nextImg}>
+        <img src={nextBtn} alt='next button' />
       </button>
     </div>
   )
