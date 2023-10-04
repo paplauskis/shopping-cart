1dfc17b18b4d04d250e198313fe0c36cc94359e4
paplauskis <paplauskaskostas@gmail.com>
2023-10-04 18:27:14 +0300

    created Shop page

---
+++ src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import ProductList from '../components/productList/ProductList'
+import NavBar from '../components/navBar/NavBar'
+
+function Products() {
+  return (
+    <>
+      <NavBar />
+      <ProductList />
+    </>
+  )
+}
+
+export default Products
