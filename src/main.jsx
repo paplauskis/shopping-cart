4231a12fcbff9454998871571f1ea5d9c19d3fef
paplauskis <paplauskaskostas@gmail.com>
2023-10-04 16:44:53 +0300

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Products from './pages/Products'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/products',
+    element: <Products />,
+  },
+  // {
+  //   path: '/cart',
+  //   element: <Cart />,
+  // },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
