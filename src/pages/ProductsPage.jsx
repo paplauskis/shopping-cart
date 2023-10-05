e51503951636307d9109e8d0211f3ebc84c021cb
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 15:47:09 +0300

    imported all needed components

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import ProductList from '../components/productList/ProductList'
+import NavBar from '../components/navBar/NavBar'
+
+function ProductsPage() {
+  return (
+    <>
+      <NavBar />
+      <ProductList />
+    </>
+  )
+}
+
+export default ProductsPage
