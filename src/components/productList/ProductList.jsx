cd5b2ebc7cf1f49cea30ed753c856b030d77dc9c
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 12:27:42 +0300

    added ItemCard component for better reusability

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import style from './ProductList.module.css'
+import ItemCard from '../itemCard/ItemCard'
 
 function ProductList() {
   const [items, setItems] = useState([])
@@ -17,14 +18,7 @@ function ProductList() {
       <div className={style.productsList}>
       {items.map((item, index) => {
         return (
-          <div className={style.product} key={index}>
-            <img src={item.image} alt='product' />
-            <p className={style.price}>$ {item.price}</p>
-            <p className={style.title}>{item.title}</p>
-            <p className={style.rating}>Rating: <span>{item.rating.rate}/5</span></p>
-            <p className={style.count}>Reviews: <span>{item.rating.count}</span></p>
-            <button className={style.addToCart}>Add to cart <input type="number" name="item_amount" id="item-amount" /></button>
-          </div>
+          <ItemCard key={index} item={item} />
         )
       })}
       </div>
