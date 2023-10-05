07626f4f6a980f5121e1e282cc40bd5e3e662134
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 15:14:17 +0300

    added basic error page

---
+++ src/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import errorPage from './ErrorPage.module.css'
+
+function ErrorPage() {
+  return (
+    <div className={errorPage.container}>
+      <h1>Oh no, this route doesn't exist!</h1>
+      <Link to="/">
+        <span className={errorPage.homeLink}>You can go back to the home page by clicking here, though!</span>
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
