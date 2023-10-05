7f77eb388a8cfb9ab116a09c43a06dbdec03829f
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 15:43:51 +0300

    set up all needed router paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-import Products from './pages/Products'
+import ProductsPage from './pages/ProductsPage'
+import ErrorPage from './pages/errorPage/ErrorPage'
+import CartPage from './pages/cart/CartPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/products',
-    element: <Products />,
+    element: <ProductsPage />,
   },
-  // {
-  //   path: '/cart',
-  //   element: <Cart />,
-  // },
+  {
+    path: '/cart',
+    element: <CartPage />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
