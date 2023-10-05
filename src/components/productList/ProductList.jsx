fe2cc1d6b220a3788bf9d458d6ae13634f223a91
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 19:02:56 +0300

    added loading div and error handling

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,26 +1,30 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 import style from './ProductList.module.css'
 import ItemCard from '../itemCard/ItemCard'
 
 function ProductList() {
   const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch('https://fakestoreapi.com/products', { mode: 'cors' })
       .then(res => res.json())
       .then(json => {
         setItems(json)
         console.log(json)
       })
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
   }, [])
 
   return (
     <>
       <div className={style.productsList}>
-      {items.map((item, index) => {
-        return (
-          <ItemCard key={index} item={item} />
-        )
-      })}
+        {loading === true
+          ? <div className={style.loading}>Loading...</div> 
+          : items.map((item, index) => {
+              return <ItemCard key={index} item={item} addItems={addItemsToCart} />
+            })}
       </div>
     </>
   )
