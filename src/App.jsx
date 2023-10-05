b7333b7dfb029698d99269770b35e620f12be231
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 15:43:14 +0300

    imported all needed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import NavBar from './components/navBar/NavBar'
+import Description from './components/description/Description'
+import PhotoReel from './components/photoReel/PhotoReel'
+import ScrollBar from './components/scrolling-bar/Scrolling-bar'
 import './App.css'
 
 function App() {
-  
+
+  return (
+    <>
+    <NavBar />
+    <ScrollBar />
+    <Description />
+    <PhotoReel />
+    </>
+  )
 }
 
 export default App
