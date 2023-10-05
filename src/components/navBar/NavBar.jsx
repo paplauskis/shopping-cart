4114b1cf2ce96811aa1860108dff9d977ac57909
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 15:13:12 +0300

    added div that tracks items in cart

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import navBar from './NavBar.module.css'
 import cartImage from '../../assets/cart.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function NavBar() {
+  const [itemCount, setItemcount] = useState(0)
   return (
     <header>
       <h1>
@@ -11,7 +13,10 @@ function NavBar() {
       <div className={navBar.links}>
         <Link to='products'>Shop</Link>
         <Link to='cart'>
-          <img src={cartImage} alt='shopping cart icon' />
+          <div className={navBar.cart}>
+            <img src={cartImage} alt='shopping cart icon' />
+            {itemCount > 0 && <div className={navBar.count}>{itemCount}</div>}
+          </div>
         </Link>
       </div>
     </header>
