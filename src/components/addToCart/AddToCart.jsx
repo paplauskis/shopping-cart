a30283f7a80198b6b1afd9887e5bc7d6ce10fa51
paplauskis <paplauskaskostas@gmail.com>
2023-10-05 14:35:29 +0300

    added functionality to buttons

--- src/components/addToCart/AddToCart.jsx
+++ src/components/addToCart/AddToCart.jsx
@@ -1,16 +1,39 @@
 import cart from './AddToCart.module.css'
+import { useState } from 'react'
 
 function AddToCart({ index }) {
+  const [amount, setAmount] = useState(1)
+  const [amountButtons, showAmountButtons] = useState(false)
+  const [cartButtonText, changeCartButtontext] = useState('Add to cart')
+
+  const toggleAmountButtons = () => {
+    if (amountButtons) {
+      showAmountButtons(false)
+      changeCartButtontext('Add to cart')
+    } else {
+      showAmountButtons(true)
+      changeCartButtontext('Confirm')
+      setAmount(1)
+    }
+  }
+
   return (
-    <button className={cart.addToCart}>
-      Add to cart{' '}
-      <input
-        type='number'
-        name='item_amount'
-        id={index}
-        className={cart.itemAmount}
-      />
-    </button>
+    <div className={cart.addButton}>
+      <button className={cart.addToCart} onClick={toggleAmountButtons}>
+        {cartButtonText}
+      </button>
+      {amountButtons && (
+        <div className={cart.amountButtons}>
+          <button className={cart.plus} onClick={() => setAmount(amount + 1)}>
+            +
+          </button>
+          <div className={cart.amount}>{amount}</div>
+          <button className={cart.minus} onClick={() => setAmount(amount - 1)}>
+            -
+          </button>
+        </div>
+      )}
+    </div>
   )
 }
 
