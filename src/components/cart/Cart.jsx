25c8600fa5ce13b59886f8b4f29bc4a0bb17871c
paplauskis <paplauskaskostas@gmail.com>
2023-10-06 22:07:35 +0300

    created div that displays items in cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react'
 import cart from './Cart.module.css'
 
-function Cart() {
+function Cart({ cartItems }) {
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  useEffect(() => {
+    let total = 0
+    cartItems.forEach(item => {
+      total += item.price * item.amount
+    })
+    setTotalPrice(total)
+  }, [])
+    
+
   return (
     <div className={cart.container}>
       <h2 className={cart.title}>Your Cart</h2>
       <hr />
+      <div className={cart.items}>
+        {cartItems.map((item, index) => {
+          return (
+            <div className={cart.item} key={index}>
+              <img src={item.image} alt='shopping cart item' />
+              <div className={cart.itemInfo}>
+                <p className={cart.title}>{item.title}</p>
+                <p className={cart.price}>$ {item.price}</p>
+                <p className={cart.amount}>Quantity: <span>{item.amount}</span></p>
+              </div>
+            </div>
+          )
+        })}
+        <p className={cart.total}>Total: $ {totalPrice}</p>
+      </div>
     </div>
   )
 }
