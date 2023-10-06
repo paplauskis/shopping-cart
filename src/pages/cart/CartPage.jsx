4c1500503321a3bbe5667d088b0fa6a2f1f4deaf
paplauskis <paplauskaskostas@gmail.com>
2023-10-06 22:06:01 +0300

    imported neeeded components and added item array

---
+++ src/pages/cart/CartPage.jsx
@@ -0,0 +1,21 @@
+import NavBar from '../../components/navBar/NavBar'
+import Cart from '../../components/cart/Cart'
+import { cartItemsArr } from '../../components/addToCart/AddToCart.jsx'
+import { useEffect, useState } from 'react'
+
+function CartPage() {
+  const [cartCount, setCartCount] = useState(cartItemsArr.length)
+
+  useEffect(() => {
+    setCartCount(cartItemsArr.length)
+  } , [])
+
+  return (
+    <>
+      <NavBar cartItemCount={cartCount}/>
+      <Cart cartItems={cartItemsArr}/>
+    </>
+  )
+}
+
+export default CartPage
