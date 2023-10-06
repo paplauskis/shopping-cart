0abbd57b27ff122f6acbe5404dcd879c24a011ae
paplauskis <paplauskaskostas@gmail.com>
2023-10-06 15:43:37 +0300

    added prop that checks item count in cart

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,21 +1,20 @@
 import navBar from './NavBar.module.css'
 import cartImage from '../../assets/cart.png'
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
 
-function NavBar() {
-  const [itemCount, setItemcount] = useState(0)
+function NavBar({ cartItemCount }) {
+
   return (
     <header>
       <h1>
         <Link to='/'>RNDMshop</Link>
       </h1>
       <div className={navBar.links}>
-        <Link to='products'>Shop</Link>
-        <Link to='cart'>
+        <Link to='/products'>Shop</Link>
+        <Link to='/cart'>
           <div className={navBar.cart}>
             <img src={cartImage} alt='shopping cart icon' />
-            {itemCount > 0 && <div className={navBar.count}>{itemCount}</div>}
+            {cartItemCount > 0 && <div className={navBar.count}>{cartItemCount}</div>}
           </div>
         </Link>
       </div>
