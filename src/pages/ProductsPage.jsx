0b32070293429b98039ef34d63ff2b6f25107bc5
paplauskis <paplauskaskostas@gmail.com>
2023-10-06 15:38:59 +0300

    added useEffect that checks for new items in cart
    also added prop that goes to NavBar

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,10 +1,31 @@
 import ProductList from '../components/productList/ProductList'
 import NavBar from '../components/navBar/NavBar'
+import { cartItemsArr } from '../components/addToCart/AddToCart'
+import { useEffect, useState } from 'react'
+
 
 function ProductsPage() {
+  const [cartCount, setCartCount] = useState(cartItemsArr.length)
+
+  useEffect(() => {
+    //interval checks every 100ms if there's a new item
+     const interval = setInterval(() => {
+      setCartCount(cartItemsArr.length)
+    }, 100);
+
+    // const secondInterval = setInterval(() => {
+    //   console.log(cartItemsArr)
+    // }, 2000);
+
+    return () => {
+      clearInterval(interval)
+      // clearInterval(secondInterval)
+    }
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <NavBar cartItemCount={cartCount}/>
       <ProductList />
     </>
   )
